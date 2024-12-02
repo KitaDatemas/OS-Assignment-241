88a5a6879daf80729fe9e6220b13df1a7bcf1fd0
Ho Quoc Dat <dat.hoquocmt@gmail.com>
2024-12-02 18:58:30 +0700

    comment out

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ASUS/OneDrive/Documents/Hệ điều hành/BTL
+# Install script for directory: C:/Users/ASUS/OneDrive/Documents/Hệ điều hành/OS-Assignment-241
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ASUS/OneDrive/Documents/Hệ điều hành/BTL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ASUS/OneDrive/Documents/Hệ điều hành/OS-Assignment-241/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
