e3a837d9f93e12ec8141f59682f3debfe6415b08
PhucTruong <truongdinhphuc1523@gmail.com>
2024-12-04 18:01:28 +0700

    update file

--- BTL/ossim_source_code/src/mm.c
+++ BTL/ossim_source_code/src/mm.c
@@ -348,8 +348,8 @@ int init_mm(struct mm_struct *mm, struct pcb_t *caller)
   vma1->vm_end= vma1->vm_start;
   vma1->sbrk = vma1->vm_start;
 
-  struct vm_rg_struct *first_rg = init_vm_rg(vma1->vm_start, vma1->vm_end, 0);
-  enlist_vm_rg_node(&vma1->vm_freerg_list, first_rg);
+  struct vm_rg_struct *first_rg_vma_1 = init_vm_rg(vma1->vm_start, vma1->vm_end, 0);
+  enlist_vm_rg_node(&vma1->vm_freerg_list, first_rg_vma_1);
 
   vma1->vm_next= NULL;
   // vma1->vm_id = ...
